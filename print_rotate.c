d57b6a9151957b57aef9efb3d282a1caa1bcc304
nohaelnagar <eng.noha17@gmail.com>
2023-08-26 00:08:05 +0200

    'print_rotate'

---
+++ print_rotate.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rotate- print rotate string
+ * @args: args to get next arg string
+ * @flags: flags for format string
+ * Return: number of characters printed
+ */
+int print_rotate(va_list *args, format_flags *flags)
+{
+	(void) args;
+	(void) flags;
+	return (0);
+}
